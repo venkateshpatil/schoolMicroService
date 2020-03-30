189571ce1612f1333255c8c887f6dda2aaf6859a
venkatesh <venkateshpatil40@gmail.com>
2020-03-29 19:18:39 -0700

    zool api gateway added without eureka client

---
+++ zoolgateway/src/test/java/com/microservicepractice/zoolgateway/ZoolgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicepractice.zoolgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoolgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
